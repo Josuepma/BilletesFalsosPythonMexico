b941beed3733e9c7a59bde3c267b6f891ee980ec
Josuepma <1930168@upv.edu.mx>
2022-04-18 21:49:20 -0500

    de a 20 o de a 50

--- detector.py
+++ detector.py
@@ -6,52 +6,106 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 import os
 
-def validacion(edge,edgec2):
-    MIN_MATCH_COUNT = 8
-    resultados=[]
-    kp1, des1 = sift.detectAndCompute(edge,None)
-    kp2, des2 = sift.detectAndCompute(edgec2,None)
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    search_params = dict(checks = 50)
-    flann = cv.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des1,des2,k=2)
-    # store all the good matches as per Lowe's ratio test.
-    good = []
-    for m,n in matches:
-        if m.distance < 0.6*n.distance:
-            good.append(m)
-    if len(good)>MIN_MATCH_COUNT:
-        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
-        matchesMask = mask.ravel().tolist()
-        h,w = edge.shape
-        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv.perspectiveTransform(pts,M)
-        img2 = cv.polylines(edgec2,[np.int32(dst)],True,255,3, cv.LINE_AA)
-    else:
-        print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+billete_opcion = input('Billete de 20 o 50?')
+if billete_opcion == '50':
+    def validacion(edge,edgec2):
+        MIN_MATCH_COUNT = 8
+        resultados=[]
+        kp1, des1 = sift.detectAndCompute(edge,None)
+        kp2, des2 = sift.detectAndCompute(edgec2,None)
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1,des2,k=2)
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+            if m.distance < 0.6*n.distance:
+                good.append(m)
+        if len(good)>MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+            matchesMask = mask.ravel().tolist()
+            h,w = edge.shape
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv.perspectiveTransform(pts,M)
+            img2 = cv.polylines(edgec2,[np.int32(dst)],True,255,3, cv.LINE_AA)
+        else:
+            print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
 
-        matchesMask = None
-    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-    print(draw_params)
-    if(matchesMask!=None):
+            matchesMask = None
+        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                    singlePointColor = None,
+                    matchesMask = matchesMask, # draw only inliers
+                    flags = 2)
+        print(draw_params)
+        if(matchesMask!=None):
+            
+            None
+        else:
+            matchesMask=[0,0]
+        if(len(matchesMask)>=3):
+            resultados+=["1"]  
+        else:
+            resultados+=["0"]
+
+        edgec2 = cv.drawMatches(edge,kp1,edgec2,kp2,good,None,**draw_params)
+        plt.imshow(edgec2, 'gray'),plt.show()
+        return resultados
+elif billete_opcion == '20':
+    def validacion(edge,edgec2):
+        MIN_MATCH_COUNT = 8
+        resultados=[]
+        kp1, des1 = sift.detectAndCompute(edge,None)
+        kp2, des2 = sift.detectAndCompute(edgec2,None)
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1,des2,k=2)
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+            if m.distance < 0.6*n.distance:
+                good.append(m)
+        if len(good)>MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+            matchesMask = mask.ravel().tolist()
+            h,w = edge.shape
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv.perspectiveTransform(pts,M)
+            img2 = cv.polylines(edgec2,[np.int32(dst)],True,255,3, cv.LINE_AA)
+        else:
+            #print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+            matchesMask = None
+        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                    singlePointColor = None,
+                    matchesMask = matchesMask, # draw only inliers
+                    flags = 2)
+        #print(len(good))
+        if(matchesMask!=None):
+            print(len(matchesMask))
+        else:
+            matchesMask=[0,0]
+
+        if(len(good)>=2):
+            resultados+=["1"]  
+        else:
+            resultados+=["0"]
         
-        None
-    else:
-        matchesMask=[0,0]
-    if(len(matchesMask)>=3):
-        resultados+=["1"]  
-    else:
-        resultados+=["0"]
+        #print(draw_params)
+        # edgec2 = cv.drawMatches(edge,kp1,edgec2,kp2,good,None,**draw_params)
+        #plt.imshow(edgec2, 'gray'),plt.show()
+        return resultados
+else:
+    print("ingrese una opción valida")
+    exit()
 
-    edgec2 = cv.drawMatches(edge,kp1,edgec2,kp2,good,None,**draw_params)
-    plt.imshow(edgec2, 'gray'),plt.show()
-    return resultados
+
 cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("test")
